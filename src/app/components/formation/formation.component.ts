fd311d094848f5e0cd91af26c7e1310801b21238
JoPrevot <jordan.prevot@soprasteria.com>
2025-02-13 15:12:29 +0100

    Mise en forme responsive + ajout

--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -21,25 +21,29 @@ export class FormationComponent implements OnInit {
       date: '2022',
       titre: 'POEI Java Full Stack',
       universite: 'M2I Formation & Sopra Steria Group',
-      description: 'Formation de reconvertion orientée Fullstack (Java Angular), méthodologie Agile et travail de groupe sur un projet fil rouge.'
+      description: 'Formation de reconvertion orientée Fullstack (Java, Angular) avec une approche Agile. Projet fil rouge avec développement d\'une application complète.',
+      competences: 'Java, Angular, Agile, Gestion de projet, Développement logiciel.'
     },
     {
       date: '2016 - 2020',
       titre: 'Doctorat en chimie des Matériaux',
       universite: 'Université de Nouvelle-Calédonie <br>Institut des Sciences Exactes et Appliquées - Nouméa',
-      description: 'Thèse scientifique menée en laboratoire (Nouméa et La Rochelle) via un financement ministériel.'
+      description: 'Thèse scientifique menée en laboratoire (Nouméa et La Rochelle) via un financement ministériel.',
+      competences: 'Chimie des matériaux, Analyse de données, Modélisation, Publication scientifique.'
     },
     {
       date: '2014 - 2016',
       titre: 'Master professionnel en Ingénierie de la Chimie',
       universite: 'Université Polytechnique Hauts-de-France - Valenciennes',
-      description: 'Master effectué en apprentissage au centre de R&D de Vallourec.'
+      description: 'Master effectué en apprentissage au centre de R&D de Vallourec.',
+      competences: 'Chimie, Recherche et Développement, Analyse de données, Gestion de projet.'
     },
     {
       date: '2013 - 2014',
       titre: 'Licence professionnelle Analyse, Contrôle et Expertise',
       universite: 'Université de Lille 1 - Villeneuve d\'Ascq',
-      description: ''
+      description: '',
+      competences: ''
     }
   ]
 
--- src/app/components/loisirs/loisirs.component.ts
+++ src/app/components/loisirs/loisirs.component.ts
@@ -17,20 +17,31 @@ import {Button} from "primeng/button";
 export class LoisirsComponent {
   loisirs = [
     {
-      name: 'Volley-Ball', description: 'Pratique en compétitions régionales / nationales depuis 2010.<br>' +
+      name: 'Volley-Ball', description: '<strong>Poste :</strong> Réceptionneur-Attaquant<br>' +
+        'Pratique en compétitions régionales / nationales depuis 2010.<br>' +
         'Capitanat, entraineur et bénévole.<br>' +
-        '<strong>Poste :</strong> Réceptionneur-Attaquant'
+        'Le volleyball me pousse à me surpasser tout en cultivant l\'esprit d\'équipe 🏐🔥'
       , icon: 'sports_volleyball'
     },
     {
-      name: 'Randonnée', description: 'Randonnées en bivouac sur plusieurs jours, GR.<br>' +
-        '<strong>Top rando: </strong>Mont Humboldt - Nouvelle Calédonie', icon: 'hiking'
+      name: 'Randonnée',
+      description: '<strong>Top rando: </strong><a href="https://balades-naturalistes.fr/le-mont-humboldt/">Mont Humboldt - Nouvelle Calédonie</a><br>' +
+        'Randonnées en bivouac sur plusieurs jours, GR.<br>' +
+        'Rien de mieux qu’une bonne rando pour souffler, se vider la tête et se challenger ! 🌿🏔️<br>'
+      ,
+      icon: 'hiking'
     },
-    {name: 'Cuisine', description: '<strong>Plat favori: </strong>Les lasagnes !', icon: 'cookie'},
+    {
+      name: 'Cuisine',
+      description: '<strong>Plat favori: </strong>Les lasagnes 🍴<br>' +
+        'J\'adore cuisiner, mais surtout manger ! <br> La gourmandise est effectivement l\'un de mes plus grand défauts 😋',
+      icon: 'cookie'
+    },
     {
       name: 'Jeux de société',
-      description: 'Grand fan de jeux de plateau et de TCG, notamment Magic The Gathering.<br>' +
-        '<strong>Jeux favoris :</strong> Wingspan, Terraforming Mars, Seasons '
+      description: '<strong>Jeux favoris :</strong> Wingspan, Terraforming Mars, Seasons <br>' +
+          'Grand fan de jeux de plateau et de TCG, notamment Magic The Gathering.<br>' +
+          'La fatigue mentale d\'une partie de Dune équivaut à une journée de randonnée 😁'
       ,
       icon: 'space_dashboard'
     },
@@ -39,7 +50,7 @@ export class LoisirsComponent {
       description: 'Passionné de romans de science-fiction et de fantasy, Tolkien, Herbert et Asimov comptent parmi mes auteurs favoris.',
       icon: 'auto_stories'
     },
-    {name: 'Jardinage', description: 'Production bio garantie', icon: 'yard'}
+    {name: 'Jardinage', description: 'Production bio garantie 🍅', icon: 'yard'}
 
   ];
 
